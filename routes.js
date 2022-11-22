c03e329e4fee0b5b1bb0dc6560f61054495b9c46
dayanidi <93707264+dayanid@users.noreply.github.com>
2022-11-23 00:36:02 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
 module.exports = app => {
-    require("./app/modules/accounts/accounts.routes")(app);
+
+    require("./app/modules/accounts/accounts.routes")(app); 
+
+    require("./app/modules/company/company.routes")(app);
 };
